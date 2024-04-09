2af8343399267969105551d4583cb8645663a784
Acemany <116539353+acemany@users.noreply.github.com>
2024-04-09 07:47:03 +0300

    Smile smile smile
    12 HOURS TO JUST IMPLEMENT SMILES HAHAHAHAHHA help me

--- main.gd
+++ main.gd
@@ -1,109 +1,108 @@
 extends Node
-#https://quizizz.com/admin/quiz/65c39f2dd1c17c3bc760d373/edit?at=65c3ac4ce48800675b7cb78a
 
-@onready var message_container = "ScrollContainer/VBoxContainer"
-@onready var scroll_container = $ScrollContainer
-@onready var button_scroll = $ButtonScroll
-@onready var line_edit = $LineEdit
+@onready var button_scroll = $Main/Head/ButtonScroll
+@onready var channel_header = $Main/Head/ChannelHeader
+@onready var channel_counter = $Main/Head/ChannelCounter
+@onready var scroll_container = $Main/Body
+@onready var message_container = $Main/Body/Messages
+@onready var line_edit = $Main/Footer/LineEdit
 
-var socket = WebSocketPeer.new()
-var started = false
-var messages = []
-var message = ""
-var jmessage = {}
-var channel_id = CHANEL_ID
+@export var user_id: int
+@export var user_token: String
+#            Acemany   Degrabebs
+@export_enum("207157", "190739") var channel_id: String = "207157"
 
-var bronzeNick = "e7820a"
-var silverNick = "b4b4b4"
-var goldNick = "eefc08"
-var chatYellow = "F2F932"
-var diamondNick = "8781bd"
-var kingNick = "30d5c8"
-var topNick = "3BCBFF"
-var darkPink = "BD70D7"
-var undeadNick = "AB4873"
-var premiumNick = "31a93a"
-var moderatorNick = "ec4058"
-var streamerNick = "e8bb00"
+
+var smile_loader = HTTPRequest.new()
+var socket: WebSocketPeer = WebSocketPeer.new()
+
+var started: bool = false
+var messages: Array = []
+
+var bronze_nick = "e7820a"
+var silver_nick = "b4b4b4"
+var gold_nick = "eefc08"
+var chat_yellow = "f2f932"
+var diamond_nick = "8781bd"
+var king_nick = "30d5c8"
+var top_nick = "3bCbff"
+var dark_pink = "bd70d7"
+var undead_nick = "ab4873"
+var premium_nick = "31a93a"
+var moderator_nick = "ec4058"
+var streamer_nick = "e8bb00"
 
 
 func _ready():
-	socket.connect_to_url("wss://chat-1.goodgame.ru/chat2/")
-	print("!STARTED!")
+	add_child(smile_loader)
+	smile_loader.connect("request_completed", _http_request_completed)
+	#smile_loader.request("https://goodgame.ru/images/smiles/DegraBebs/%s.png" % smile)
 	
-	for i in messages:
-		var messa = RichTextLabel.new()
-		messa.custom_minimum_size = Vector2(1000, 24)
-		messa.autowrap_mode = TextServer.AUTOWRAP_WORD
-		messa.bbcode_enabled = true
-		var usercolor = ""
-		if jmessage['data']['color'] == "premium-personal":
-			usercolor = "purple"
-		elif jmessage['data']['color'] == "simple":
-			usercolor = "green"
-		else:
-			usercolor = "gray"
-		messa.text = "[color=%s]%s[/color]: %s" % [usercolor, jmessage['data']['user_name'], jmessage['data']['text']]
-		get_node(message_container).add_child(messa)
+	socket.connect_to_url("wss://chat-1.goodgame.ru/chat2/")
+	print("STARTED")
 
 func _process(_delta):
-	
 	socket.poll()
 	var state = socket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
-		if not started:7e062991eae9061d8367f82cb1a7a4ad
+		if !started:
 			started = true
-			socket.send_text(str({"type": "auth",
-								 "data": {"user_id":INSERT_USERNAME,
-										  "token":"INSERT_USER_TOKEN",
-										  "object":"",
-										  "active":true}}))
-			socket.send_text(str({"type": "join",
-								  "data": {"channel_id": "channel_id",
-										   "hidden": 0,
-										   "mobile": false,
-										   "reload": false}}))
-			socket.send_text(str({"type": "get_channel_history",
-								  "data": {"channel_id": "channel_id"}}))
+			wss_start()
 		else:
 			while socket.get_available_packet_count():
-				message = socket.get_packet().get_string_from_utf8()
-				jmessage = JSON.parse_string(message)
-				debuglog(message)
-				if jmessage["type"] == "message":
-					logf("New message from %s(%s): %s" % [jmessage['data']['user_name'], jmessage['data']['user_id'], jmessage['data']['text']], 0)
-					print("%s: %s" % [jmessage['data']['user_name'], jmessage['data']['text']])
-					append_message(jmessage)
-				elif jmessage["type"] == "error":
-					logf("Wss error{}" % jmessage['data'], 2)
-					print("%s(%s)" % [jmessage['data']['errorMsg'], jmessage['data']])
-				elif jmessage["type"] == "channel_counters":
-					logf("%s clients and %s users in channel'%s'" % [jmessage['data']['clients_in_channel'], jmessage['data']['users_in_channel'], jmessage['data']["channel_id"]])
-				elif jmessage["type"] == "channel_history":
-					logf("Getting history of channel[%s] - %s" % [jmessage['data']["channel_id"], str(len(jmessage["data"]["messages"]))])
-					for i in jmessage["data"]["messages"]:
-						append_message(i, true)
-				else:
-					logf("Unknown message: %s" % message, 1)
-					print("Received message: %s" % message)
+				var message = socket.get_packet().get_string_from_utf8()
+				var json_message = JSON.parse_string(message)
+				var message_type: String = json_message["type"]
+				var message_data: Dictionary = json_message["data"]
+				print(message_data)
+				match message_type:
+					"success_join":
+						channel_header.clear()
+						channel_header.append_text("[center]%s[/center]" % message_data['channel_name'])
+						line_edit.editable = not message_data['premium_only'] as bool
+						channel_counter.text = "ጰ %s 🤖 %s" % [message_data['users_in_channel'], message_data['clients_in_channel']-message_data['users_in_channel']]
+					"channel_history":
+						for i in message_data['messages']:
+							append_message(i)
+					"message":
+						append_message(message_data)
+					"error":
+						logf("Error%s" % message_data, 2)
+						append_message({
+							"text": message_data['errorMsg'],
+							"user_name": "ERROR",
+							"color": "undead",
+							"user_id": 0,
+							"message_id": INF,
+							"timestamp": Time.get_unix_time_from_system()})
+					"channel_counters":
+						channel_counter.text = "ጰ %s 🤖 %s" % [message_data['users_in_channel'], message_data['clients_in_channel']-message_data['users_in_channel']]
+					_:
+						logf("Unknown message: %s" % message, 1)
+						print("Received message: %s" % message)
 			if button_scroll.button_pressed:
 				scroll_container.scroll_vertical += 2
 	elif state == WebSocketPeer.STATE_CLOSING:
 		logf("Closing")
-		print("!CLOSING!")
+		print("Closing...")
 	elif state == WebSocketPeer.STATE_CLOSED:
 		var code = socket.get_close_code()
 		var reason = socket.get_close_reason()
-		print("!CLOSED WITH CODE!: %d, reason %s. Clean: %s" % [code, reason, code != -1])
-		set_process(false)
+		print("CLOSED WITH CODE: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		get_tree().quit()
 
 func _physics_process(_delta):
 	messages.sort_custom(id_sort)
 
-func _on_line_edit_text_submitted(new_text):
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		socket.close(1000, "Esc. pressed")
+
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
 	socket.send_text(str({"type": "send_message",
 						  "data": {
-							  "channel_id": "channel_id",
+							  "channel_id": channel_id,
 							  "text": new_text,
 							  "color": "premium",
 							  "icon": "top1",
@@ -112,68 +111,99 @@ func _on_line_edit_text_submitted(new_text):
 	line_edit.clear()
 
 
-func separate_every(a: String, b: float):
-	var aaa = []
-	for i in range(ceil((len(a)-1)/b)):
-		aaa.append([]) #[[] for i in range(ceil((len(rtext)-1)/b))]
-	for i in range(len(a)):
-		aaa[floor(i/b)].append(a[i])
-	var aa = range(len(aaa))
-	aa.fill("")
-	for i in len(aaa):
-		for j in aaa[i]:
-			aa[i] += j
-	return aa
+func wss_start() -> void:
+			socket.send_text(str({"type": "auth",
+								  "data":{
+									"user_id": user_id,
+									"token": user_token,
+									"object": "",
+									"active": true}}))
+			socket.send_text(str({"type": "join",
+								  "data": {
+									"channel_id": channel_id,
+									"hidden": 0,
+									"mobile": false,
+									"reload": false}}))
+			socket.send_text(str({"type": "get_channel_history",
+								  "data": {
+									"channel_id": channel_id}}))
 
-func time_sort(a, b):
+func separate_every(string: String, freq: float) -> Array[String]:
+	var preseparated: Array = []
+	for i in range(ceil((len(string)-1)/freq)):
+		preseparated.append([])
+		
+	for i in range(len(string)):
+		preseparated[floor(i/freq)].append(string[i])
+	var separated = range(len(preseparated))
+	separated.fill("")
+	
+	for i in len(preseparated):
+		for j in preseparated[i]:
+			separated[i] += j
+	return separated
+
+func time_sort(a: Dictionary, b: Dictionary) -> bool:
 	return Time.get_unix_time_from_datetime_string(a["time"]) < Time.get_unix_time_from_datetime_string(b["time"])
 
-func id_sort(a, b):
+func id_sort(a: Dictionary, b: Dictionary) -> bool:
 	return a["message_id"] < b["message_id"]
 
-func append_message(jsonmessage, listin:bool = false):
-	if not listin:
-		jsonmessage = jsonmessage["data"]
-	var messa = RichTextLabel.new()
-	messa.custom_minimum_size = Vector2(1152, 1 * 24)
-	messa.autowrap_mode = TextServer.AUTOWRAP_WORD
-	var usercolor = ""
-	if jsonmessage['color'] == "premium-personal":
-		usercolor = premiumNick
-	elif jsonmessage['color'] == "simple":
-		usercolor = topNick
-	elif jsonmessage['color'] == "undead":
-		usercolor = undeadNick
-	elif jsonmessage['color'] == "diamond":
-		usercolor = diamondNick
-	elif jsonmessage['color'] == "gold":
-		usercolor = goldNick
-	elif jsonmessage['color'] == "silver":
-		usercolor = silverNick
-	elif jsonmessage['color'] == "bronze":
-		usercolor = bronzeNick
-	elif jsonmessage['color'] == "premium":
-		usercolor = premiumNick
-	elif jsonmessage['color'] == "king":
-		usercolor = kingNick
-	elif jsonmessage['color'] == "streamer":
-		usercolor = streamerNick
-	elif jsonmessage['color'] == "gradient-fire":
-		usercolor = chatYellow
-	elif jsonmessage['color'] == "":
-		usercolor = premiumNick
-	else:
-		print(jsonmessage['color'])
-		usercolor = "#000000"
-	messa.append_text("[color=%s]%s[/color]: %s" % [usercolor, jsonmessage['user_name'], jsonmessage['text']])
-	get_node(message_container).add_child(messa)
-	messages.append({"text": jsonmessage['text'],
-					 "user_name": jsonmessage['user_name'],
-					 "user_id": jsonmessage['user_id'],
-					 "message_id": jsonmessage['message_id'],
-					 "time": Time.get_datetime_string_from_unix_time(jsonmessage['timestamp']+10800)})
+func append_message(jmessage: Dictionary) -> void:
+	var message: RichTextLabel = RichTextLabel.new()
+	message.fit_content = true
+	
+	var usercolor: String = ""
+	match jmessage['color']:
+		"premium-personal":
+			usercolor = premium_nick
+		"simple":
+			usercolor = top_nick
+		"undead":
+			usercolor = undead_nick
+		"diamond":
+			usercolor = diamond_nick
+		"gold":
+			usercolor = gold_nick
+		"silver":
+			usercolor = silver_nick
+		"bronze":
+			usercolor = bronze_nick
+		"premium":
+			usercolor = premium_nick
+		"king":
+			usercolor = king_nick
+		"streamer":
+			usercolor = streamer_nick
+		"gradient-fire":
+			usercolor = chat_yellow
+		"":
+			usercolor = premium_nick
+		_:
+			print("Unsupported color \"%s\"" % jmessage['color'])
+			usercolor = "#ff00ff"
+	
+	var message_text: String = jmessage['text']
+	for i in range(1, 49):
+		var smilec = "degrabebs%s" % i
+		message_text = message_text.replace(":%s:" % smilec, "[img]res://cache/%s.png[/img]" % smilec)
+	message_text = message_text.replace(":gg:", "[img]res://cache/gg.png[/img]")
 
-func logf(err: String, warn: int = 0):
+	message.append_text("[color=%s]%s[/color]: %s" % [usercolor, jmessage['user_name'], message_text])
+	message_container.add_child(message)
+	messages.append({"text": jmessage['text'],
+					 "user_name": jmessage['user_name'],
+					 "user_id": jmessage['user_id'],
+					 "message_id": jmessage['message_id'],
+					 "timestamp": Time.get_datetime_string_from_unix_time(jmessage['timestamp']+10800)})
+
+func _http_request_completed(_result, _response_code, _headers, body):
+	var image = Image.new()
+	print(image.load_png_from_buffer(body))
+	print("Code %s" % image.save_png("res://cache/name.png"))
+
+
+func logf(err: String, warn: int = 0) -> void:
 	var warn_level = ""
 	if warn > 1:
 		warn_level = 'E'
@@ -182,9 +212,4 @@ func logf(err: String, warn: int = 0):
 	else:
 		warn_level = 'I'
 	var file = FileAccess.open("res://log.txt", FileAccess.READ_WRITE)
-	file.store_string("%s\n[%s]-%s:\n%s\n" % [file.get_as_text(), warn_level, str(Time.get_datetime_string_from_system()), err])
-
-func debuglog(txt: String):
-	"Log.\ntxt - error text."
-	var file = FileAccess.open("res://logg.txt", FileAccess.READ_WRITE)
-	file.store_string("%s\n%s:\n%s\n" % [file.get_as_text(), str(Time.get_datetime_string_from_system()), txt])
+	file.store_string("%s\n[%s]-%s:\n%s\n" % [file.get_as_text(), warn_level, Time.get_datetime_string_from_system(), err])
